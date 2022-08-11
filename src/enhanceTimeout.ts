a02d2f47911a9620f13e88f21ec3a3337fa201fa
Oleksandra <alexafinas@gmail.com>
2022-08-11 13:07:51 +0200

    v1.0.1 fix main entry

---
+++ src/enhanceTimeout.ts
@@ -0,0 +1,27 @@
+type Timer = ReturnType<typeof setTimeout> | undefined;
+
+/**
+ * Helper that allows to track timerID
+ */
+export const enhanceTimeout = (): {
+  getTimer: () => Timer;
+  clearTimer: Function;
+  setTimer: Function;
+} => {
+  let timerId: Timer;
+
+  return {
+    getTimer: () => timerId,
+    clearTimer: () => {
+      if (timerId) {
+        clearTimeout(timerId);
+        timerId = undefined;
+      }
+    },
+    setTimer: (timerFn: Function, time: number) => {
+      timerId = setTimeout(timerFn, time) as unknown as Timer;
+    },
+  };
+};
+
+export default enhanceTimeout;
